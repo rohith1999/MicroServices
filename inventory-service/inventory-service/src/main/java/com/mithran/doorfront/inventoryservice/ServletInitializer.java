6ee5802d1a7f1c02eec08ac35807f6fed169d4e0
rohith1999 <rohithsuri3@gmail.com>
2022-02-09 13:08:33 +0530

    java version 1.8

---
+++ inventory-service/inventory-service/src/main/java/com/mithran/doorfront/inventoryservice/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.mithran.doorfront.inventoryservice;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(InventoryServiceApplication.class);
+	}
+
+}
